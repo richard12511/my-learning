db41db735a484a3dc223903a8b33e5178dbc7e4a
Richard Schmidt <richardnew12511@gmail.com>
2024-08-24 11:38:58 -0500

    structs

---
+++ elixir/elixir_in_action/abstractions/todo_list/lib/todo_list.ex
@@ -0,0 +1,15 @@
+defmodule TodoList do
+  defstruct next_id: 1, entries: %{}
+
+  def new, do: %TodoList{}
+
+  def add_entry(todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.next_id)
+
+    updated_entries = Map.put(todo_list.entries, todo_list.next_id, entry)
+    %TodoList{todo_list | entries: updated_entries, next_id: todo_list.next_id + 1 } 
+  end
+
+  def entries(list, date) do
+  end
+end
