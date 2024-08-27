c0bc080b5e036ac316eb0448eeb38346337a7a55
Richard Schmidt <richardnew12511@gmail.com>
2024-08-27 15:31:31 -0500

    implement collectable

--- elixir/elixir_in_action/abstractions/todo_list/lib/todo_list.ex
+++ elixir/elixir_in_action/abstractions/todo_list/lib/todo_list.ex
@@ -51,3 +51,20 @@ defmodule TodoList.CsvImporter do
   end
 
 end
+
+defimpl Collectable, for: TodoList do
+  def into(original) do
+    { original, &into_callback/2 }
+  end
+
+  defp into_callback(todo_list, {:cont, entry}), do: TodoList.add_entry(todo_list, entry)
+  defp into_callback(todo_list, :done), do: todo_list
+  defp into_callback(_todo_list, :halt), do: :ok
+
+end
+
+defimpl String.Chars, for: TodoList do
+  def to_string(_) do
+    "#TodoList"
+  end
+end
